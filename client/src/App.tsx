ee6669bb574fa6b5ac60df9926ec1cb4fbbb283d
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-10-22 22:09:16 -0300

    :heavy_plus_sign: Added TailwindCSS
    Also was added configuration dependencies like CRACO, PostCSS and others

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-gray-700 flex items-center justify-center">
+      <h1 className="font-bold text-white text-6xl">✅ To-do App</h1>
     </div>
   );
 }
